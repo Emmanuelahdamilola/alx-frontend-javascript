69d6c0f096b2b4872167771e88d958bc5761bcf8
Emmanuelah Damilola <emmanuelahdamilola1992@gmail.com>
2024-02-07 19:52:43 +0000

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,21 +3,16 @@ import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   try {
-    // Using Promise.allSettled to handle both promises concurrently
-    const [userResult, photoResult] = await Promise.allSettled([
-      signUpUser(firstName, lastName),
-      uploadPhoto(fileName),
-    ]);
+    const userPromise = signUpUser(firstName, lastName);
+    const photoPromise = uploadPhoto(fileName);
 
-    // Creating the result array based on promise statuses
-    const resultArray = [
+    const [userResult, photoResult] = await Promise.allSettled([userPromise, photoPromise]);
+
+    return [
       { status: userResult.status, value: userResult.value },
       { status: photoResult.status, value: photoResult.value },
     ];
-
-    return resultArray;
   } catch (error) {
-    // Handle any unexpected errors during the process
     return [{ status: 'rejected', value: error.message }];
   }
 }
