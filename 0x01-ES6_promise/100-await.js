fa99a15348645e0bd6a0200dbe132ec382073fff
Emmanuelah Damilola <emmanuelahdamilola1992@gmail.com>
2024-02-07 18:53:04 +0000

    Await / Async

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,23 @@
+import { uploadPhoto, createUser } from './utils';
+
+async function asyncUploadUser() {
+  try {
+    const [photoResponse, userResponse] = await Promise.all([
+      uploadPhoto(),
+      createUser(),
+    ]);
+
+    return {
+      photo: photoResponse,
+      user: userResponse,
+    };
+  } catch (error) {
+    console.error(error); // Log the error for debugging purposes
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
+
+export default asyncUploadUser;
